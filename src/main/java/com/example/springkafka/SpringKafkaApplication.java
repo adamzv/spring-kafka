6e0c45b0927bd605d9d319a06f2bbef49ddcd9a6
Adam Zverka <31664842+adamzv@users.noreply.github.com>
2023-06-13 21:07:38 +0200

    Updated kafka configuration

--- src/main/java/com/example/springkafka/SpringKafkaApplication.java
+++ src/main/java/com/example/springkafka/SpringKafkaApplication.java
@@ -3,22 +3,15 @@ package com.example.springkafka;
 import com.github.javafaker.Faker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationStartedEvent;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
-import reactor.util.function.Tuple2;
 
 import java.time.Duration;
-import java.util.function.Function;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -28,15 +21,15 @@ public class SpringKafkaApplication {
         SpringApplication.run(SpringKafkaApplication.class, args);
     }
 
-    @Bean
-    NewTopic aircraft2() {
-        return TopicBuilder.name("aircraft2").partitions(1).replicas(3).build();
-    }
+    // @Bean
+    // NewTopic aircraft2() {
+    // return TopicBuilder.name("aircraft2").partitions(1).replicas(3).build();
+    // }
 
-    @Bean
-    NewTopic esports() {
-        return TopicBuilder.name("esports").partitions(1).replicas(3).build();
-    }
+    // @Bean
+    // NewTopic esports() {
+    // return TopicBuilder.name("esports").partitions(1).replicas(3).build();
+    // }
 
 }
 
@@ -67,7 +60,7 @@ class Producer {
 @Slf4j
 class Consumer {
 
-    // @KafkaListener(topics = {"aircraft"}, groupId = "spring-boot-kafka")
+    @KafkaListener(topics = { "my-topic" }, groupId = "spring-boot-kafka")
     public void consume(ConsumerRecord<Integer, String> record) {
         log.info("received = [{}, {}]", record.key(), record.value());
     }
